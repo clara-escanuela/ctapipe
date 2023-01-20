cb1ac6147cffd9003ab2085bb3b87a6473dcc70d
clara-escanuela <53437218+clara-escanuela@users.noreply.github.com>
2023-01-20 16:33:30 +0000

    Deploying to gh-pages from  @ 17b438098ab9560a4a19477e3e3a08dd7cceb946 🚀

---
+++ _static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '0.16.1.dev506+g17b43809.d20230120',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
