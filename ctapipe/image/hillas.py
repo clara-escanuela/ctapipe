a08d442d9faf2261476f8d030cb6e95931b8aec2
nieves <clara.escanuela@mpi-hd.mpg.de>
2023-01-31 19:03:04 +0100

    solved issue

--- ctapipe/image/hillas.py
+++ ctapipe/image/hillas.py
@@ -215,116 +215,101 @@ def hillas_parameters(geometry, dl1_image, cleaned_mask, pedestal_variance, gaus
             kurtosis=kurtosis_long,
         )
 
+    #Add rows of pixels to the cleaned image
+
+    n = 2
+    m = cleaned_mask.copy()
+    for ii in range(n):
+        m = dilate(geometry, m)
+
+    mask = np.array((m.astype(int) + cleaned_mask.astype(int)), dtype=bool)
+    cleaned_image = dl1_image.copy()
+    cleaned_image[~mask] = 0.0
+    cleaned_image[cleaned_image<0] = 0.0
+    size = np.sum(cleaned_image)
+
+    def fit(z, xi, yi, image, pedestal_variance, emf=1.585):
+        delta_x = (xi - z[0])
+        delta_y = (yi - z[1])
+
+        longitudinal = delta_x * np.cos(z[2]) + delta_y * np.sin(z[2])
+        transverse = delta_x * np.sin(z[2]) - delta_y * np.cos(z[2])
+
+        x = (longitudinal/z[3])**2
+        y = (transverse/z[4])**2
+
+        prediction = (z[5])*np.exp(-(1/2)*(x + y))
+
+        chi_square = chi_squared(cleaned_image, prediction, pedestal_variance, emf)
+
+        return chi_square
+
+    x0 = [cog_x, cog_y, psi, length, width, np.max(cleaned_image)/(2*np.pi*cog_x*cog_y)]
+
+    bnds = ((-1.2, 1.2), (-1.2, 1.2), (-1.572, 1.572), (0, 1), (0, 1), (0, np.max(cleaned_image)))
+
+    result = opt.minimize(fit, x0=x0, args=(geometry.pix_x.value, geometry.pix_y.value, cleaned_image, pedestal_variance), bounds=bnds)
+    results = result.x
+
+    fit_xcog = results[0]
+    fit_ycog = results[1]
+    fit_psi = results[2]
+    fit_length = results[3]
+    fit_width = results[4]
+
+    fit_rcog = np.linalg.norm([fit_xcog, fit_ycog])
+    fit_phi = np.arctan2(fit_ycog, fit_xcog)
+
+    if fit_length <= 0 or fit_width <= 0 or fit_length>1 or fit_width>0.5:
+        fit_xcog = cog_x
+        fit_ycog = cog_y
+        fit_rcog = cog_r
+        fit_phi = cog_phi
+        fit_length = length
+        fit_width = width
+        fit_psi = psi
+
+    delta_x = geometry.pix_x.value - fit_xcog
+    delta_y = geometry.pix_y.value - fit_ycog
+
+    cov = np.cov(delta_x, delta_y, aweights=cleaned_image, ddof=0)
+    eig_vals, eig_vecs = np.linalg.eigh(cov)
+
+    longitudinal = delta_x * np.cos(fit_psi) + delta_y * np.sin(fit_psi)
+
+    m3_long = np.average(longitudinal**3, weights=cleaned_image)
+    skewness_long = m3_long / fit_length**3
+
+    m4_long = np.average(longitudinal**4, weights=cleaned_image)
+    kurtosis_long = m4_long / fit_length**4
+
+    cos_2psi = np.cos(2 * fit_psi)
+    a = (1 + cos_2psi) / 2
+    b = (1 - cos_2psi) / 2
+    c = np.sin(2 * fit_psi)
+
+    A = ((delta_x**2.0) - cov[0][0]) / size
+    B = ((delta_y**2.0) - cov[1][1]) / size
+    C = ((delta_x * delta_y) - cov[0][1]) / size
+
+    if fit_length == 0:
+        length_uncertainty = np.nan
     else:
-        
-        n = 3
-        m = cleaned_mask.copy()
-        for ii in range(n):
-            m = dilate(geometry, m)
+        length_uncertainty = np.sqrt(
+            np.sum(((((a * A) + (b * B) + (c * C))) ** 2.0) * cleaned_image)
+        ) / (2 * fit_length)
 
-        mask = np.array((m.astype(int) + cleaned_mask.astype(int)), dtype=bool)
-
-        cleaned_image = dl1_image.copy()
-        cleaned_image[~mask] = 0.0
-        cleaned_image[cleaned_image<0] = 0.0
-
-        def fit(z, xi, yi, image, pedestal_variance, amplitude, emf=1.585):
-            delta_x = (xi - z[0])
-            delta_y = (yi - z[1])
-
-            longitudinal = delta_x * np.cos(z[2]) + delta_y * np.sin(z[2])
-            transverse = delta_x * np.sin(z[2]) - delta_y * np.cos(z[2])
-
-            x = (longitudinal/z[3])**2
-            y = (transverse/z[4])**2
-
-            prediction = (1/(2*np.pi*z[3]*z[4]))*np.exp(-(1/2)*(x + y))
-
-            chi_square = chi_squared(cleaned_image, prediction, pedestal_variance, emf)
-
-            return chi_square
-
-        x0 = [cog_x, cog_y, psi, length, width]
-
-        bnds = ((-1.2, 1.2), (-1.2, 1.2), (-1.572, 1.572), (0, 2), (0, 2))
-
-        result = opt.minimize(fit, x0=x0, args=(geometry.pix_x.value, geometry.pix_y.value, cleaned_image/np.sum(cleaned_image), pedestal_variance, size), method='Nelder-Mead', bounds=bnds)
-        results = result.x
-
-        fit_xcog = results[0]
-        fit_ycog = results[1]
-        fit_psi = results[2]
-        fit_length = results[3]
-        fit_width = results[4]
-
-        fit_rcog = np.linalg.norm([fit_xcog, fit_ycog])
-        fit_phi = np.arctan2(fit_ycog, fit_xcog)
-
-        if fit_length <= 0 or fit_width <= 0:
-            fit_xcog = cog_x
-            fit_ycog = cog_y
-            fit_rcog = cog_r
-            fit_phi = cog_phi
-            fit_length = length
-            fit_width = width
-            fit_psi = psi
-
-        delta_x = geometry.pix_x.value - fit_xcog
-        delta_y = geometry.pix_y.value - fit_ycog
-
-        cov = np.cov(delta_x, delta_y, aweights=cleaned_image, ddof=0)
-        eig_vals, eig_vecs = np.linalg.eigh(cov)
-
-        longitudinal = delta_x * np.cos(fit_psi) + delta_y * np.sin(fit_psi)
-
-        m3_long = np.average(longitudinal**3, weights=cleaned_image)
-        skewness_long = m3_long / fit_length**3
-
-        m4_long = np.average(longitudinal**4, weights=cleaned_image)
-        kurtosis_long = m4_long / fit_length**4
-
-        cos_2psi = np.cos(2 * fit_psi)
-        a = (1 + cos_2psi) / 2
-        b = (1 - cos_2psi) / 2
-        c = np.sin(2 * fit_psi)
-
-        A = ((delta_x**2.0) - cov[0][0]) / size
-        B = ((delta_y**2.0) - cov[1][1]) / size
-        C = ((delta_x * delta_y) - cov[0][1]) / size
-
-        if fit_length == 0:
-            length_uncertainty = np.nan
-        else:
-            length_uncertainty = np.sqrt(
-                np.sum(((((a * A) + (b * B) + (c * C))) ** 2.0) * cleaned_image)
-            ) / (2 * fit_length)
-
-        if fit_width == 0:
-            width_uncertainty = np.nan
-        else:
-            width_uncertainty = np.sqrt(
-                np.sum(((((b * A) + (a * B) + (-c * C))) ** 2.0) * cleaned_image)
-                    ) / (2 * fit_width)
-       
-        size = np.sum(cleaned_image)
-        if unit.is_equivalent(u.m):
-            return CameraHillasParametersContainer(
-                x=u.Quantity(fit_xcog, unit),
-                y=u.Quantity(fit_ycog, unit),
-                r=u.Quantity(fit_rcog, unit),
-                phi=Angle(fit_phi, unit=u.rad),
-                intensity=size,
-                length=u.Quantity(fit_length, unit),
-                length_uncertainty=u.Quantity(length_uncertainty, unit),
-                width=u.Quantity(fit_width, unit),
-                width_uncertainty=u.Quantity(width_uncertainty, unit),
-                psi=Angle(fit_psi, unit=u.rad),
-                skewness=skewness_long,
-                kurtosis=kurtosis_long,
-                )
-        return HillasParametersContainer(
-            fov_lon=u.Quantity(fit_xcog, unit),
-            fov_lat=u.Quantity(fit_ycog, unit),
+    if fit_width == 0:
+        width_uncertainty = np.nan
+    else:
+        width_uncertainty = np.sqrt(
+            np.sum(((((b * A) + (a * B) + (-c * C))) ** 2.0) * cleaned_image)
+                ) / (2 * fit_width)
+    
+    if unit.is_equivalent(u.m):
+        return CameraHillasParametersContainer(
+            x=u.Quantity(fit_xcog, unit),
+            y=u.Quantity(fit_ycog, unit),
             r=u.Quantity(fit_rcog, unit),
             phi=Angle(fit_phi, unit=u.rad),
             intensity=size,
@@ -336,4 +321,18 @@ def hillas_parameters(geometry, dl1_image, cleaned_mask, pedestal_variance, gaus
             skewness=skewness_long,
             kurtosis=kurtosis_long,
             )
+    return HillasParametersContainer(
+        fov_lon=u.Quantity(fit_xcog, unit),
+        fov_lat=u.Quantity(fit_ycog, unit),
+        r=u.Quantity(fit_rcog, unit),
+        phi=Angle(fit_phi, unit=u.rad),
+        intensity=size,
+        length=u.Quantity(fit_length, unit),
+        length_uncertainty=u.Quantity(length_uncertainty, unit),
+        width=u.Quantity(fit_width, unit),
+        width_uncertainty=u.Quantity(width_uncertainty, unit),
+        psi=Angle(fit_psi, unit=u.rad),
+        skewness=skewness_long,
+        kurtosis=kurtosis_long,
+        )
 
